07e476cf56bec44fafc66161db0072cc596f27ec
Wiwiweb <wiwiweb2@gmail.com>
2013-06-18 17:08:52 -0700

    Added video posts, fixed bugs

--- src/sakuraibot.py
+++ src/sakuraibot.py
@@ -16,8 +16,9 @@ import urllib2
 from bs4 import BeautifulSoup
 from time import sleep
 import datetime
+import smtplib
 
-VERSION = "1.0"
+VERSION = "1.1"
 FREQUENCY = 300
 SAKURAI_BABBLE = "Sakurai: (laughs)"
 
@@ -39,7 +40,7 @@ else:
 if debug:
     logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format='%(asctime)s: %(message)s')
 else:
-    logging.basicConfig(filename='sakurai.log', level=logging.INFO, format='%(asctime)s: %(message)s')
+    logging.basicConfig(filename='sakuraibot.log', level=logging.INFO, format='%(asctime)s: %(message)s')
 
 logging.info("--- Starting sakuraibot ---")    
     
@@ -52,10 +53,11 @@ cookie = cookief.read().strip()
 cookief.close()
 
 class PostDetails:
-    def __init__(self, author, text, picture):
+    def __init__(self, author, text, picture, video):
         self.author = author
         self.text = text
         self.picture = picture
+        self.video = video
 
 def getMiiverseLastPost():
     """Fetches the URL path to the last Miiverse post in the Director's room."""
@@ -63,7 +65,6 @@ def getMiiverseLastPost():
     req.add_header("Cookie", cookie)
     page = urllib2.urlopen(req).read()
     soup = BeautifulSoup(page)
-    print page
     post_url = soup.find("div", {"class":"post"}).get("data-href")
     logging.info("Last post found: " + post_url)
     return post_url
@@ -72,14 +73,20 @@ def getMiiverseLastPost():
 def isNewPost(post_url):
     """Compares the latest post URL to the ones we already processed, to see if it is new."""
     postf = open(LAST_POST_FILENAME, "r")
-    last_post = postf.read()
+    
+    # We have to check 50 posts, because sometimes Miiverse will mess up the order and we might think it's a new post even though it's not.
+    for _ in range(49): # Only 50 posts on the first page.
+        seen_post = postf.readline().strip()
+        if seen_post == post_url:
+            postf.close()
+            logging.info("Post was already posted")
+            return False
+        elif not seen_post: # No more lines.
+            break
+
     postf.close()
-    if last_post == post_url:
-        logging.info("Post was already posted")
-        return False
-    else:
-        logging.info("Post is new!")
-        return True
+    logging.info("Post is new!")
+    return True
 
 
 def getInfoFromPost(post_url):
@@ -97,39 +104,55 @@ def getInfoFromPost(post_url):
     
     screenshot_container = soup.find("div", {"class":"screenshot-container"})
     if screenshot_container == None:
+        # Text post
         picture_url = None
-        logging.info("No picture found.")
+        video_url = None
+        logging.info("No picture or video found.")
+    elif "video" in screenshot_container["class"]:
+        # Video post
+        picture_url = None
+        video_url = soup.find("p", {"class":"url-link"}).find("a").get("href")
+        logging.info("Post video: " + video_url)
     else:
+        # Picture post
         picture_url = screenshot_container.find("img").get("src")
+        video_url = None
         logging.info("Post picture: " + picture_url)
     
-    return PostDetails(author, text, picture_url)
+    return PostDetails(author, text, picture_url, video_url)
     
 def postToReddit(post_details):
     """Posts the new Miiverse post to /r/smashbros"""
-    user_agent = "SakuraiBot v" + VERSION + " by /u/Wiwiweb"
+    user_agent = "SakuraiBot v" + VERSION + " by /u/Wiwiweb for /r/smashbros"
     r = praw.Reddit(user_agent=user_agent)
     r.login(USERNAME, password)
     logging.info("Logged into Reddit.")
     
-    title = "New " + post_details.author + " post! \"" + post_details.text + "\""
+    date = datetime.datetime.now().strftime("%y-%m-%d")
     if post_details.picture == None:
         # Self post
-        title = "New " + post_details.author + " post! (" + datetime.datetime.now().strftime("%y-%m-%d") + ") \"" + post_details.text + "\" (No picture)"
+        title = "New " + post_details.author + " post! (" + date + ") \"" + post_details.text + "\" (No picture)"
         submission = r.submit(subreddit, title, text=SAKURAI_BABBLE)
         logging.info("New self-post posted! " + submission.short_link)
     else:
         # Link post
-        title = "New " + post_details.author + " picture! \"" + post_details.text + "\""
-        submission = r.submit(subreddit, title, url=post_details.picture)
+        if post_details.video == None:
+            title = "New " + post_details.author + " picture! (" + date + ") \"" + post_details.text + "\""
+            submission = r.submit(subreddit, title, url=post_details.picture)
+        else:
+            title = "New " + post_details.author + " video! (" + date + ") \"" + post_details.text + "\""
+            submission = r.submit(subreddit, title, url=post_details.video)
         logging.info("New submission posted!" + submission.short_link)
 
 
 def setLastPost(post_url):
-    """Replaces the last post remembered with the argument."""
-    postf = open(LAST_POST_FILENAME, "w")
-    postf.write(post_url)
+    """Adds the last post remembered with the argument."""
+    postf = open(LAST_POST_FILENAME, "r+")
+    old = postf.read()
+    postf.seek(0)
+    postf.write(post_url + "\n" + old)
     postf.close()
+    logging.info("New post remembered.")
 
     
 
@@ -137,7 +160,7 @@ def setLastPost(post_url):
 try:
     while True:
         try:
-            
+               
             logging.info("Starting the cycle again.")
             post_url = getMiiverseLastPost()
             if isNewPost(post_url):
@@ -145,7 +168,7 @@ try:
                 postToReddit(post_details)
                 if not debug:
                     setLastPost(post_url)
-            
+               
             if debug:
                 quit()
         except urllib2.HTTPError as e:
@@ -154,6 +177,7 @@ try:
             logging.info("URLError: " + e.reason + ". Sleeping another iteration and retrying.")
         except Exception as e:
             logging.info("Unknown error: " + str(e) + ". Sleeping another iteration and retrying.")
+              
         sleep(FREQUENCY)
         
 except (KeyboardInterrupt):
