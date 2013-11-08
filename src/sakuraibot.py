05af4c115af44edcdbf45bb5f2399b4973700836
Wiwiweb <wiwiweb2@gmail.com>
2013-11-08 12:13:58 -0800

    Fix bug in set_last_char()

--- src/sakuraibot.py
+++ src/sakuraibot.py
@@ -499,7 +499,7 @@ class SakuraiBot:
 
     def set_last_char(self, new_char_id):
         """Add the char id to the last-char.txt file."""
-        postf = open(self.last_post_filename, 'w')
+        postf = open(self.last_char_filename, 'w')
         postf.write('characters/' + new_char_id + '.html')
         postf.close()
         self.logger.info("New char remembered.")
